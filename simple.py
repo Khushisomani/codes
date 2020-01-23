b6baf878cc9059d134f7910787f43e29f416146a
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-01-23 14:04:41 +0530

    The Hurdle Race
    Dan is playing a video game in which his character competes in a hurdle race. Hurdles are of varying heights, and Dan has a maximum height he can jump. There is a magic potion he can take that will increase his maximum height by  unit for each dose. How many doses of the potion must he take to be able to jump all of the hurdles.
    
    Given an array of hurdle heights , and an initial maximum height Dan can jump, , determine the minimum number of doses Dan must take to be able to clear all the hurdles in the race.
    
    For example, if  and Dan can jump  unit high naturally, he must take  doses of potion to be able to jump all of the hurdles.

--- simple.py
+++ simple.py
@@ -1,42 +1,35 @@
 #!/bin/python3
 
+import math
 import os
+import random
+import re
 import sys
 
-#
-# Complete the getMoneySpent function below.
-#
-def getMoneySpent(keyboards, drives, b):
-   max1=-1
-   for i in range(len(keyboards)):
-       for j in range(len(drives)):
-           if keyboards[i]+drives[j]>max1 and keyboards[i]+drives[j]<=b:
-               max1=keyboards[i]+drives[j]
-    
-   return(max1)
-
+# Complete the hurdleRace function below.
+def hurdleRace(k, height):
+    p=0
+    for i in range(len(height)):
+        if height[i]<=k:
+            continue
+        else:
+            if height[i]-k>p:
+                p=height[i]-k
+    return(p)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    bnm = input().split()
+    nk = input().split()
 
-    b = int(bnm[0])
+    n = int(nk[0])
 
-    n = int(bnm[1])
+    k = int(nk[1])
 
-    m = int(bnm[2])
+    height = list(map(int, input().rstrip().split()))
 
-    keyboards = list(map(int, input().rstrip().split()))
+    result = hurdleRace(k, height)
 
-    drives = list(map(int, input().rstrip().split()))
-
-    #
-    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
-    #
-
-    moneySpent = getMoneySpent(keyboards, drives, b)
-
-    fptr.write(str(moneySpent) + '\n')
+    fptr.write(str(result) + '\n')
 
     fptr.close()
