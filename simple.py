534be9978f2d6c69a8c887907f7292685be12ec8
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-01-24 15:09:30 +0530

    Find Digits
    An integer  is a divisor of an integer  if the remainder of .
    
    Given an integer, for each digit that makes up the integer determine whether it is a divisor. Count the number of divisors occurring within the integer.

--- simple.py
+++ simple.py
@@ -6,39 +6,32 @@ import random
 import re
 import sys
 
-# Complete the jumpingOnClouds function below.
-def jumpingOnClouds(c, k):
-    a=100
-    i=k
-    while(i!=0):
-        if c[i]==1:
-            a=a-3 
-        else:
-            a=a-1
-        if(i+k>len(c)-1):
-            b=len(c)-i
-            i=k-b
-        else:
-            i=i+k 
-    if c[0]==0:
-        a=a-1
-    else:
-        a=a-3
-    return(a)
+# Complete the findDigits function below.
+def findDigits(n):
+    l=[]
+    k=n
+    while(n>0):
+        a=n%10
+        l.append(a)
+        n=n//10
+    count=0
+    for i in range(len(l)):
+        if l[i]!=0:
+            if k%l[i]==0:
+                count+=1
+    return(count)
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    nk = input().split()
+    t = int(input())
 
-    n = int(nk[0])
+    for t_itr in range(t):
+        n = int(input())
 
-    k = int(nk[1])
+        result = findDigits(n)
 
-    c = list(map(int, input().rstrip().split()))
-
-    result = jumpingOnClouds(c, k)
-
-    fptr.write(str(result) + '\n')
+        fptr.write(str(result) + '\n')
 
     fptr.close()
