480d89b477b56098026c6b21f3436cfe3c6fe1be
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-01-28 16:10:33 +0530

    Migratory Birds
    You have been asked to help study the population of birds migrating across the continent. Each type of bird you are interested in will be identified by an integer value. Each time a particular kind of bird is spotted, its id number will be added to your array of sightings. You would like to be able to find out which type of bird is most common given a list of sightings. Your task is to print the type number of that bird and if two or more types of birds are equally common, choose the type with the smallest ID number.
    
    For example, assume your bird sightings are of types . There are two each of types  and , and one sighting of type . Pick the lower of the two types seen twice: type .

--- simple.py
+++ simple.py
@@ -1,57 +1,9 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'taumBday' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER b
-#  2. INTEGER w
-#  3. INTEGER bc
-#  4. INTEGER wc
-#  5. INTEGER z
-#
-
-def taumBday(b, w, bc, wc, z):
-    cost=0
-    if wc+z>bc:
-        cost=b*bc
-    else:
-        cost=b*(wc+z)
-    if bc+z>wc:
-        cost+=w*wc
-    else:
-        cost+=w*(bc+z)
-    return(cost)
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    t = int(input().strip())
-
-    for t_itr in range(t):
-        first_multiple_input = input().rstrip().split()
-
-        b = int(first_multiple_input[0])
-
-        w = int(first_multiple_input[1])
-
-        second_multiple_input = input().rstrip().split()
-
-        bc = int(second_multiple_input[0])
-
-        wc = int(second_multiple_input[1])
-
-        z = int(second_multiple_input[2])
-
-        result = taumBday(b, w, bc, wc, z)
-
-        fptr.write(str(result) + '\n')
-
-    fptr.close()
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+a = ar.count(1)
+b = ar.count(2)
+c = ar.count(3)
+d = ar.count(4)
+e = ar.count(5)
+li=[a,b,c,d,e]
+print(1+li.index(max(li)))
