182a1e76965d08998251e7d38eb6bbf7b14e8848
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-02-06 18:19:57 +0530

    Minimum Distances
    We define the distance between two array values as the number of indices between the two values. Given , find the minimum distance between any pair of equal elements in the array. If no such value exists, print -1

--- hackerrank.py
+++ hackerrank.py
@@ -6,14 +6,29 @@ import random
 import re
 import sys
 
-# Complete the extraLongFactorials function below.
-def extraLongFactorials(n):
-    fact=1
-    while(n>0):
-        fact=fact*n
-        n=n-1
-    print(fact)
+# Complete the minimumDistances function below.
+def minimumDistances(a):
+    l=[]
+    for i in range(len(a)):
+        for j in range(i+1,len(a)):
+            if a[i]==a[j] and a[j]!='a':
+                l.append(j-i)
+                a[j]='a'
+    if len(l)>0:
+        return(min(l))
+    else:
+        return(-1)
+
+
 if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
     n = int(input())
 
-    extraLongFactorials(n)
+    a = list(map(int, input().rstrip().split()))
+
+    result = minimumDistances(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
