eab3bbb253f3f1aff99b4adc7675fd7ea2d76ad5
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-07-13 19:48:39 +0530

    Greedy Florist
    A group of friends want to buy a bouquet of flowers. The florist wants to maximize his number of new customers and the money he makes. To do this, he decides he'll multiply the price of each flower by the number of that customer's previously purchased flowers plus 1.

---
+++ Greedy Florist.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(k, c):
+    c.sort(reverse=True)
+    result = 0
+    p = 1
+    for n,i in enumerate(c,start=1):
+        result+=p*i
+        if n%k==0:
+            p+=1
+    return(result)
+
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
