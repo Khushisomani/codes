eb756f756daa612b6cbe175f8e27de0efa936dbe
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-09-26 00:14:48 +0530

    Magic Pairs
    Alexandra has some distinct integer numbers a1,a2...an. Count number of pairs (i,j) such that:
    
    1≤ i ≤ n
    1≤ j ≤ n
    ai < aj

---
+++ 38181884.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().split()))
+    print(n*(n-1)//2)
+                
+            
+        
+        
