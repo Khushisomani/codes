e93daa4dadcdd1243133ddc852121cdaa38e8f5f
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-09-26 00:25:07 +0530

    GCD2
    Frank explained its friend Felman the algorithm of Euclides to calculate the GCD of two numbers. Then Felman implements it algorithm
    
    int gcd(int a, int b)
    {
    	if (b==0)
    		return a;
    	else
    		return gcd(b,a%b);
    }
    and it proposes to Frank that makes it but with a little integer and another integer that has up to 250 digits.
    Your task is to help Frank programming an efficient code for the challenge of Felman.

---
+++ 38182069.py
@@ -0,0 +1,10 @@
+# cook your dish here
+def gcd(a,b):
+    if b==0:
+        return a 
+    else:
+        return gcd(b,a%b)
+    
+for _ in range(int(input())):
+    a,b=map(int,input().split())
+    print(gcd(a,b))
