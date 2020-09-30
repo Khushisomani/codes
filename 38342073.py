990e87d89fab545f811662e156a91c916d803595
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-09-30 22:33:30 +0530

    Andrew and the Meatballs
    Andrew likes meatballs very much.
    
    He has N plates of meatballs, here the ith plate contains Pi meatballs. You need to find the minimal number of plates Andrew needs to take to his trip to Las Vegas, if he wants to eat there at least M meatballs. Note that each plate is already packed, i.e. he cannot change the amount of meatballs on any plate.

---
+++ 38342073.py
@@ -0,0 +1,19 @@
+# cook your dish here
+for _ in range(int(input())):
+    n,m=map(int,input().split())
+    l=list(map(int,input().split()))
+    a=0
+    count=0
+    l.sort()
+    for i in range(len(l)):
+        a+=l[n-i-1]
+        count+=1
+        if a>=m:
+            break 
+    if a>=m:
+        print(count)
+    else:
+        print(-1)
+            
+    
+            
