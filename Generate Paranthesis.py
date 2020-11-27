3de6cacefb60fd092e7a1a8e28105a2061116143
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-11-28 01:01:23 +0530

    Generate Parentheses
    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.

---
+++ Generate Paranthesis.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def generateParenthesis(self, N):
+        ans = []
+        def backtrack(S = '', left = 0, right = 0):
+            if len(S)==2*N:
+                ans.append(S)
+            if left<N:
+                backtrack(S+"(",left+1,right)
+            if right<left:
+                backtrack(S+")",left,right+1)
+        backtrack()
+        return ans
+                
