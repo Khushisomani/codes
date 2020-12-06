bb342ca84d074c084555061e652e7489fb0ca58a
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-12-06 15:29:52 +0530

    Group Anagrams
    Given an array of strings strs, group the anagrams together. You can return the answer in any order.
    
    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.

---
+++ Anagrams.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = collections.defaultdict(list)
+        for st in strs:
+            s=''.join(sorted(st))
+            dic[s].append(st)
+        return dic.values()
+        
