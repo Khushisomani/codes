665bb4e2ce8a64e892f3ef05cb1584a04ce49f0e
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-12-10 00:01:04 +0530

    Find the Duplicate Number
    Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
    
    There is only one duplicate number in nums, return this duplicate number.
    
    Follow-ups:
    
    How can we prove that at least one duplicate number must exist in nums? 
    Can you solve the problem without modifying the array nums?
    Can you solve the problem using only constant, O(1) extra space?
    Can you solve the problem with runtime complexity less than O(n2)?

--- find duplicates.py
+++ find duplicates.py
@@ -1,9 +1,11 @@
 class Solution:
-    def findDuplicates(self, nums: List[int]) -> List[int]:
-        res = []
+    def findDuplicate(self, nums: List[int]) -> int:
+        a=0
         for i in range(len(nums)):
-            if nums[abs(nums[i])-1] >= 0:
+            if nums[abs(nums[i])-1]>0:
                 nums[abs(nums[i])-1]=-nums[abs(nums[i])-1]
             else:
-                res.append(abs(nums[i]))
-        return res
+                a=abs(nums[i])
+                break
+        return a
+        
