bcc8a73ae8d2841d2cb6424a671ff0a0e908a198
Khushisomani <56465311+Khushisomani@users.noreply.github.com>
2020-12-12 17:10:07 +0530

    Basic Calculator II
    Given a string s which represents an expression, evaluate this expression and return its value. 
    
    The integer division should truncate toward zero.

---
+++ Basic Calculator.py
@@ -0,0 +1,22 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack=[]
+        sign='+'
+        num=0
+        for i in range(len(s)):
+            if 48<=ord(s[i])<=57:
+                num=num*10+int(s[i])
+            if s[i] in ['+','-','*','/'] or i+1==len(s):
+                if sign=='+':
+                    stack.append(num)
+                elif sign=='-':
+                    stack.append(-num)
+                elif sign=='*':
+                    stack[-1]*=num
+                elif sign=='/':
+                    stack[-1]=int(stack[-1]/num)
+                sign=s[i]
+                num=0
+        return sum(stack)
+                    
+            
